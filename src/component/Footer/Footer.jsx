223e9c3d3c4742302082c96af18cd9eace18bbdc
Your Name <imranislamjishan200@gmaile.com>
2023-05-04 13:50:57 +0600

    footer add done

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 const Footer = () => {
     return (
-        <div>
-            ami Footer
+        <div className='bg-red-700 h-[200px]'>
+            <div className='text-center font-semibold text-md pt-8'>
+                <Link className='text-white ml-4' to='/regestation'>Regestation</Link>
+                <Link className='text-white ml-4' to='/'>Affiliate</Link>
+                <Link className='text-white ml-4' to='/'>FAQ</Link>
+                <Link className='text-white ml-4' to='/blog'>Blogs</Link>
+            </div>
+
+            <div className='mt-6 mb-2 w-[150px] mx-auto flex justify-center items-center'>
+                <img className='w-8 h-8 mx-auto' src="../../../public/fb.png" alt="" />
+                <img className='w-8 h-8 mx-auto' src="../../../public/li.png" alt="" />
+                <img className='w-8 h-8 mx-auto' src="../../../public/insta.png" alt="" />
+                <img className='w-8 h-8 mx-auto' src="../../../public/twi.png" alt="" />
+            </div>
+
+            <p className='text-center text-white mt-4 font-semibold text-md'>© 2023. Bongo Restora. All rights reserved.</p>
         </div>
     );
 };
