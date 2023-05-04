80724f1e85e335f4c392084516f0823bd05616f0
Your Name <imranislamjishan200@gmaile.com>
2023-05-04 22:30:17 +0600

    spinner add on spingle recepi page

--- src/component/Layout/Recipi.jsx
+++ src/component/Layout/Recipi.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StarIcon, BookmarkIcon } from '@heroicons/react/24/solid'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Recipi = () => {
+const Recipi = ({ formula }) => {
+
+    console.log(formula);
+
+    const [isButtonDisabled, setButtonDesable] = useState(false)
+
+    const notify = () => {
+        toast("Add To Faviroute.");
+        setButtonDesable(true)
+    };
+
+
     return (
-        <div>
+        <div className='mb-8'>
             <div className="card w-96 bg-base-100 border-1 shadow-md">
-                <figure><img loading="lazy" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOu966OtyyOj-T02fw8WFGOUkO_z3-VT9Fjg&usqp=CAU" alt="Shoes" /></figure>
+                <figure><img loading="lazy" src={formula?.picture} alt="Shoes" /></figure>
                 <div className="card-body">
 
-                    <h2 className="card-title">recipe name</h2>
+                    <h2 className="card-title">{formula?.name}</h2>
                     <p className='mt-3'>cooking method 5 point</p>
                     <div className='flex justify-between items-center  mt-4'>
-                        <p className='inline-flex'><StarIcon className="h-6 w-6 text-red-700" />Rating: 18</p>
+                        <p className='inline-flex'><StarIcon className="h-6 w-6 text-red-700" />Rating: {formula?.rating}</p>
 
                         <div onClick={notify}>
                             <p className={`pt-2 pb-2 pl-3 pr-3 inline-flex font-semibold text-white rounded ${isButtonDisabled ? 'bg-gray-500 cursor-not-allowed' : 'bg-red-700 cursor-pointer'}`}>
--- src/component/Layout/SingleChefRecepi.jsx
+++ src/component/Layout/SingleChefRecepi.jsx
@@ -1,8 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { StarIcon, BookmarkIcon, HandThumbUpIcon, PencilSquareIcon, CalendarDaysIcon } from '@heroicons/react/24/solid'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { useParams } from 'react-router-dom';
+import Recipi from './Recipi';
 
 
 const SingleChefRecepi = () => {
@@ -20,34 +19,28 @@ const SingleChefRecepi = () => {
             })
     }, [id])
 
-    // const { name, short_bio, picture, like, number_of_recipes, experience_years } = singleData.chef;
 
 
 
-    const [isButtonDisabled, setButtonDesable] = useState(false)
 
-    const notify = () => {
-        toast("Add To Faviroute.");
-        setButtonDesable(true)
-    };
 
     return (
         <section>
             {/* Chef Conatiner */}
-            {loading ? <div>Loading...</div> :
+            {loading ? <h1 className='p-6 text-center mx-auto animate-bounce rounded-full bg-yellow-400 w-32'>Data Loding.......</h1> :
                 <div className='mt-6 mb-12'>
                     <div className="card card-side bg-base-100 shadow mr-8 ml-8">
-                        <figure><img loading="lazy" className='h-[200px]' src={singleData.chef.picture} alt=" Movie" /></figure>
+                        <figure><img loading="lazy" className='h-[200px]' src={singleData?.chef?.picture} alt=" Movie" /></figure>
 
                         <div className="card-body">
-                            <h2 className="card-title">{singleData.chef.name}</h2>
+                            <h2 className="card-title">{singleData?.chef?.name}</h2>
                             <p>{singleData.chef.short_bio}</p>
                             <div className='flex justify-around items-center'>
-                                <p className='inline-flex'><HandThumbUpIcon className="h-6 w-6 text-red-700" />Like: {singleData.chef.like}</p>
+                                <p className='inline-flex'><HandThumbUpIcon className="h-6 w-6 text-red-700" />Like: {singleData?.chef?.like}</p>
 
-                                <p className='inline-flex mt-1 mb-1'><PencilSquareIcon className="h-6 w-6 text-red-700" />Number Of Recepies: {singleData.chef.number_of_recipes}</p>
+                                <p className='inline-flex mt-1 mb-1'><PencilSquareIcon className="h-6 w-6 text-red-700" />Number Of Recepies: {singleData?.chef?.number_of_recipes}</p>
 
-                                <p className='inline-flex mb-2 mt-1'><CalendarDaysIcon className="h-6 w-6 text-red-700" />Years of experience: {singleData.chef.experience_years}</p>
+                                <p className='inline-flex mb-2 mt-1'><CalendarDaysIcon className="h-6 w-6 text-red-700" />Years of experience: {singleData?.chef?.experience_years}</p>
                             </div>
 
                         </div>
@@ -57,29 +50,9 @@ const SingleChefRecepi = () => {
             }
             {/* Chef Recepi Conainer */}
             <div className='mb-8 mr-8 ml-8 grid grid-cols-3'>
-
-
-                <div className="card w-96 bg-base-100 border-1 shadow-md">
-                    <figure><img loading="lazy" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOu966OtyyOj-T02fw8WFGOUkO_z3-VT9Fjg&usqp=CAU" alt="Shoes" /></figure>
-                    <div className="card-body">
-
-                        <h2 className="card-title">recipe name</h2>
-                        <p className='mt-3'>cooking method 5 point</p>
-                        <div className='flex justify-between items-center  mt-4'>
-                            <p className='inline-flex'><StarIcon className="h-6 w-6 text-red-700" />Rating: 18</p>
-
-                            <div onClick={notify}>
-                                <p className={`pt-2 pb-2 pl-3 pr-3 inline-flex font-semibold text-white rounded ${isButtonDisabled ? 'bg-gray-500 cursor-not-allowed' : 'bg-red-700 cursor-pointer'}`}>
-                                    Add Favourite <BookmarkIcon className="h-6 w-6 ml-1 text-white" />
-                                </p>
-                            </div>
-
-                            <ToastContainer />
-
-                        </div>
-
-                    </div>
-                </div>
+                {
+                    singleData?.recipes?.map(formula => <Recipi key={formula?.name} formula={formula} ></Recipi>)
+                }
             </div>
 
 
